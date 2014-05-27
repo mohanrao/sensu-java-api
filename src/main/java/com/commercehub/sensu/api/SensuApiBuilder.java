f1bcb67cc229ab219f53e6b7aa725546c83292a2
David M. Carr <david@carrclan.us>
2014-05-27 00:05:58 -0400

    attempt to add support for API authentication

--- src/main/java/com/commercehub/sensu/api/SensuApiBuilder.java
+++ src/main/java/com/commercehub/sensu/api/SensuApiBuilder.java
@@ -1,11 +1,27 @@
 package com.commercehub.sensu.api;
 
 import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 
 public class SensuApiBuilder {
-    public static SensuApi build(String url) {
-        return Feign.builder().decoder(new GsonDecoder()).encoder(new GsonEncoder()).errorDecoder(new SensuErrorDecoder()).target(SensuApi.class, url);
+    private Feign.Builder builder;
+
+    private SensuApiBuilder() {
+        builder = Feign.builder().decoder(new GsonDecoder()).encoder(new GsonEncoder()).errorDecoder(new SensuErrorDecoder());
+    }
+
+    public SensuApiBuilder authentication(String username, String password) {
+        builder.requestInterceptor(new BasicAuthRequestInterceptor(username, password));
+        return this;
+    }
+
+    public SensuApi build(String url) {
+        return builder.target(SensuApi.class, url);
+    }
+
+    public static SensuApiBuilder builder() {
+        return new SensuApiBuilder();
     }
 }
--- src/test/groovy/com/commercehub/sensu/api/ApiSpec.groovy
+++ src/test/groovy/com/commercehub/sensu/api/ApiSpec.groovy
@@ -4,13 +4,21 @@ import spock.lang.Specification
 
 abstract class ApiSpec extends Specification {
     protected static final ENV_SENSU_URL = "SENSU_API_URL"
+    protected static final ENV_SENSU_USERNAME = "SENSU_API_USERNAME"
+    protected static final ENV_SENSU_PASSWORD = "SENSU_API_PASSWORD"
 
     SensuApi api
 
     def setup() {
         def apiUrl = System.getenv(ENV_SENSU_URL)
         if (apiUrl) {
-            api = SensuApiBuilder.build(apiUrl)
+            def builder = SensuApiBuilder.builder()
+            def username = System.getenv(ENV_SENSU_USERNAME)
+            def password = System.getenv(ENV_SENSU_PASSWORD)
+            if (username && password) {
+                builder.authentication(username, password)
+            }
+            api = builder.build(apiUrl)
         }
     }
 }
