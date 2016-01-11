00abfe2d51c9078a5690505d2ec86241acf5ac14
Elizabeth Falkner <efalkner@mac-qa-ef.nexus.commercehub.com>
2016-01-11 15:06:23 -0500

    Update event API to reflect different check object structure

--- src/main/java/com/commercehub/sensu/api/Event.java
+++ src/main/java/com/commercehub/sensu/api/Event.java
@@ -4,17 +4,9 @@ package com.commercehub.sensu.api;
 public class Event {
     private String id;
     private Client client;
-    private Check check;
+    private EventCheck check;
     private int occurrences;
     private String action;
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
 
     public String getId() {
         return id;
@@ -32,11 +24,11 @@ public class Event {
         this.client = client;
     }
 
-    public Check getCheck() {
+    public EventCheck getCheck() {
         return check;
     }
 
-    public void setCheck(Check check) {
+    public void setCheck(EventCheck check) {
         this.check = check;
     }
 
@@ -46,6 +38,14 @@ public class Event {
 
     public void setOccurrences(int occurrences) {
         this.occurrences = occurrences;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
     }
 
     @Override
@@ -59,29 +59,5 @@ public class Event {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Event event = (Event) o;
-
-        if (occurrences != event.occurrences) return false;
-        if (id != null ? !id.equals(event.id) : event.id != null) return false;
-        if (client != null ? !client.equals(event.client) : event.client != null) return false;
-        if (check != null ? !check.equals(event.check) : event.check != null) return false;
-        return !(action != null ? !action.equals(event.action) : event.action != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (client != null ? client.hashCode() : 0);
-        result = 31 * result + (check != null ? check.hashCode() : 0);
-        result = 31 * result + occurrences;
-        result = 31 * result + (action != null ? action.hashCode() : 0);
-        return result;
-    }
 
 }
